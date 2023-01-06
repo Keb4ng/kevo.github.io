1fcacd366510331b6940c00861a244487f1b6968
Jon Trinidad <122079605+Keb4ng@users.noreply.github.com>
2023-01-06 16:06:35 +0800

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $(".slideshow").slick({
+    dots: true,
+    autoplay: true,
+    arrows: false,
+    speed: 500
+    });
+});
+
+
+ $(".ai-font-menu").click(function(){
+  $( ".mobile-menu" ).toggle( "fast", function() {
+
+    });
+  });
